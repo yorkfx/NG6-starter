40dbdd12f17a3375f31a653c5a973a14dbe2d731
Héctor Martínez <mghec83@gmail.com>
2017-05-23 21:33:53 -0500

    Integración de angular resource

--- client/app/app.js
+++ client/app/app.js
@@ -3,12 +3,14 @@ import uiRouter from 'angular-ui-router';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
+import ngResource from 'angular-resource';
 import 'normalize.css';
 
 angular.module('app', [
     uiRouter,
     Common,
-    Components
+    Components,
+    ngResource
   ])
   .config(($locationProvider) => {
     "ngInject";
