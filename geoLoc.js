1e2b7640d0f00108d8afc6809dd932e72d292f24
villachuck <carlos.villavicencio.aguila@gmail.com>
2017-05-29 21:46:45 -0500

    Add files via upload

---
+++ geoLoc.js
@@ -0,0 +1,239 @@
+var app = angular.module("app", []);
+
+app.controller("appCtrl", function ($scope) {
+
+    // current location
+    $scope.loc = { lat: 19.411560, lon: -99.155273 };
+    $scope.gotoCurrentLocation = function () {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var c = position.coords;
+                $scope.gotoLocation(c.latitude, c.longitude);
+            });
+            return true;
+        }
+        return false;
+    };
+    $scope.gotoLocation = function (lat, lon) {
+        if ($scope.lat != lat || $scope.lon != lon) {
+            $scope.loc = { lat: lat, lon: lon };
+            if (!$scope.$$phase) $scope.$apply("loc");
+        }
+    };
+
+    // geo-coding
+    $scope.search = "";
+    $scope.geoCode = function () {
+        if ($scope.search && $scope.search.length > 0) {
+            if (!this.geocoder) this.geocoder = new google.maps.Geocoder();
+            this.geocoder.geocode({ 'address': $scope.search }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var loc = results[0].geometry.location;
+                    $scope.search = results[0].formatted_address;
+                    $scope.gotoLocation(loc.lat(), loc.lng());
+                } else {
+                    alert("Sorry, this search produced no results.");
+                }
+            });
+        }
+    };
+
+ $scope.cities = [
+              {
+                  place : 'Impact Hub',
+                  desc : "Obtén hasta un 25% de descuento al preinscribirte al Batch 15",
+                  lat : 19.4175708,
+                  lon : -99.1628403
+              },
+              {
+                  place : 'Rappi',
+                  desc : 'Descarga la app y obtén $150 de descuento en tu primera compra',
+                  lat : 19.417213,
+                  lon : -99.1628897
+              },
+              {
+                  place : 'RadioShack Pabellón Cuauhtémoc',
+                  desc : 'Hasta 25% de descuento del 29 de mayo al 4 de junio',
+                  lat : 19.4113941,
+                  lon : -99.1561359
+              },
+              {
+                  place : 'Pizza del Perro Negro',
+                  desc : 'Pizza de chile relleno mediana a tan solo $275',
+                  lat : 19.417006,
+                  lon : -99.166088
+              },
+              {
+                  place : 'Moustaches Bistrot',
+                  desc : 'Camarones al ajillo (170 grs.) a $220',
+                  lat : 19.4276477,
+                  lon : -99.2065353
+              },
+              {
+                  place : 'Un Lugar de la Mancha',
+                  desc : 'Desayunos desde $199. ¡Contamos con Librería y Cafetería!',
+                  lat : 19.4277843,
+                  lon : -99.2070141
+              },
+              {
+                  place : 'Suntory Lomas',
+                  desc : 'Prueba la Tempura (camarones y verduras capeadas con harina japonesa) por solo $175. ¡Exquisito!',
+                  lat : 19.4272809,
+                  lon : -99.2057065
+              }
+
+          ];    
+});
+
+// formats a number as a latitude (e.g. 40.46... => "40°27'44"N")
+app.filter('lat', function () {
+    return function (input, decimals) {
+        if (!decimals) decimals = 0;
+        input = input * 1;
+        var ns = input > 0 ? "N" : "S";
+        input = Math.abs(input);
+        var deg = Math.floor(input);
+        var min = Math.floor((input - deg) * 60);
+        var sec = ((input - deg - min / 60) * 3600).toFixed(decimals);
+        return deg + "°" + min + "'" + sec + '"' + ns;
+    }
+});
+
+// formats a number as a longitude (e.g. -80.02... => "80°1'24"W")
+app.filter('lon', function () {
+    return function (input, decimals) {
+        if (!decimals) decimals = 0;
+        input = input * 1;
+        var ew = input > 0 ? "E" : "W";
+        input = Math.abs(input);
+        var deg = Math.floor(input);
+        var min = Math.floor((input - deg) * 60);
+        var sec = ((input - deg - min / 60) * 3600).toFixed(decimals);
+        return deg + "°" + min + "'" + sec + '"' + ew;
+    }
+});
+
+// - Documentation: https://developers.google.com/maps/documentation/
+app.directive("appMap", function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: "<div></div>",
+        scope: {
+            center: "=",        // Center point on the map (e.g. <code>{ latitude: 10, longitude: 10 }</code>).
+            markers: "=",       // Array of map markers (e.g. <code>[{ lat: 10, lon: 10, name: "hello" }]</code>).
+            width: "@",         // Map width in pixels.
+            height: "@",        // Map height in pixels.
+            zoom: "@",          // Zoom level (one is totally zoomed out, 25 is very much zoomed in).
+            mapTypeId: "@",     // Type of tile to show on the map (roadmap, satellite, hybrid, terrain).
+            panControl: "@",    // Whether to show a pan control on the map.
+            zoomControl: "@",   // Whether to show a zoom control on the map.
+            scaleControl: "@"   // Whether to show scale control on the map.
+        },
+        link: function (scope, element, attrs) {
+            var toResize, toCenter;
+            var map;
+            var currentMarkers;
+
+            // listen to changes in scope variables and update the control
+            var arr = ["width", "height", "markers", "mapTypeId", "panControl", "zoomControl", "scaleControl"];
+            for (var i = 0, cnt = arr.length; i < arr.length; i++) {
+                scope.$watch(arr[i], function () {
+                    cnt--;
+                    if (cnt <= 0) {
+                        updateControl();
+                    }
+                });
+            }
+
+            // update zoom and center without re-creating the map
+            scope.$watch("zoom", function () {
+                if (map && scope.zoom)
+                    map.setZoom(scope.zoom * 6);
+            });
+            scope.$watch("center", function () {
+                if (map && scope.center)
+                    map.setCenter(getLocation(scope.center));
+            });
+
+            // update the control
+            function updateControl() {
+
+                // update size
+                if (scope.width) element.width(scope.width);
+                if (scope.height) element.height(scope.height);
+
+                // get map options
+                var options =
+                {
+                    center: new google.maps.LatLng(23, 79),
+                    zoom: 13,
+                    mapTypeId: "roadmap"
+                };
+                if (scope.center) options.center = getLocation(scope.center);
+                if (scope.zoom) options.zoom = scope.zoom * 1;
+                if (scope.mapTypeId) options.mapTypeId = scope.mapTypeId;
+                if (scope.panControl) options.panControl = scope.panControl;
+                if (scope.zoomControl) options.zoomControl = scope.zoomControl;
+                if (scope.scaleControl) options.scaleControl = scope.scaleControl;
+
+                // create the map
+                map = new google.maps.Map(element[0], options);
+
+                // update markers
+                updateMarkers();
+
+                // listen to changes in the center property and update the scope
+                google.mapTypeIds.event.addListener(map, 'center_changed', function () {
+
+                    // do not update while the user pans or zooms
+                    if (toCenter) clearTimeout(toCenter);
+                    toCenter = setTimeout(function () {
+                        if (scope.center) {
+
+                            // check if the center has really changed
+                            if (map.center.lat() != scope.center.lat ||
+                                map.center.lng() != scope.center.lon) {
+
+                                // update the scope and apply the change
+                                scope.center = { lat: map.center.lat(), lon: map.center.lng() };
+                                if (!scope.$$phase) scope.$apply("center");
+                            }
+                        }
+                    }, 500);
+                });
+            }
+
+            // update map markers to match scope marker collection
+            function updateMarkers() {
+                if (map && scope.markers) {
+
+                    // clear old markers
+                    if (currentMarkers != null) {
+                        for (var i = 0; i < currentMarkers.length; i++) {
+                            currentMarkers[i] = m.setMap(null);
+                        }
+                    }
+
+                    // create new markers
+                    currentMarkers = [];
+                    var markers = scope.markers;
+                    if (angular.isString(markers)) markers = scope.$eval(scope.markers);
+                    for (var i = 0; i < markers.length; i++) {
+                        var m = markers[i];
+                        var loc = new google.maps.LatLng(m.lat, m.lon);
+                        var mm = new google.maps.Marker({ position: loc, map: map, title: m.name });
+                        currentMarkers.push(mm);
+                    }
+                }
+            }
+
+            // convert current location to Google maps location
+            function getLocation(loc) {
+                if (loc == null) return new google.maps.LatLng(23, 79);
+                if (angular.isString(loc)) loc = scope.$eval(loc);
+                return new google.maps.LatLng(loc.lat, loc.lon);
+            }
+        }
+    };
+});
